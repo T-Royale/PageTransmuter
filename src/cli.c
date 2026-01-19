6786b25221741f375fdd8bb7e4ec072991df63d7
T-Royale <141929186+T-Royale@users.noreply.github.com>
2026-01-19 14:14:11 +0100

    PageTransmuter 2.0-BETA
    Now, pagetransmuter is 100% a cli tool, I rewrote 90% of it using better practices and fixed various bugs, now it doesn't depend on external config files or folders and can be used in any directory

--- src/cli.c
+++ src/cli.c
@@ -1,67 +1,111 @@
-//Headers del programa
 #include "Include/Functions.h"
 
+/* cli.c - user interaction via command line
+	- parse cli flags and print help messages
+	- system errors are only reported using perror
+*/
+
 flags_t* flags_get(int argc, char **argv){
-    flags_t* result = malloc(sizeof(flags_t));
-    result->output = NULL;
-    result->input = NULL;
-    result->help = false;
-    result->version = false;
-    result->http_headers = false;
-    static struct option long_opts[] = {
-        {"output", required_argument, 0, 'o'},
-        {"file", required_argument, 0, 'f'},
-        {"help", no_argument, 0, 'h'},
-        {"version", no_argument, 0, 'v'},
-        {"http-headers", no_argument, 0, 't'},
-    };
-    int opt_index = 0;
-    int c = -1;
-    while((c = getopt_long(argc, argv, "o:f:hvt", long_opts, &opt_index)) != -1){
-        switch(c){
-            case 'o': {
-                int len = strlen(optarg)+1;
-                result->output = malloc(len * sizeof(char));
-                strncpy(result->output, optarg, len);
-                break;
-            }
-            case 'f': {
-                int len = strlen(optarg)+1;
-                result->input = malloc(len * sizeof(char));
-                strncpy(result->input, optarg, len);
-                break;
-            }
-            case 'h':
-                result->help = true;
-                break;
-            case 'v':
-                result->version = true;
-                break;
-            case 't':
-                result->http_headers = true;
-                break;
-            case '?':
-            default:
-                printf("");
-                break;
-        }
-    }
-    return result;
+	flags_t* result = malloc(sizeof(flags_t));
+	if(!result) return NULL;
+	result->output = NULL;
+	result->input = NULL;
+	result->help = false;
+	result->version = false;
+	result->http_headers = false;
+	result->macro_name = NULL;
+	static const struct option long_opts[] = {
+		{"output", required_argument, 0, 'o'},
+		{"file", required_argument, 0, 'f'},
+		{"macro-name", required_argument, 0, 'n'},
+		{"help", no_argument, 0, 'h'},
+		{"version", no_argument, 0, 'v'},
+		{"http-headers", no_argument, 0, 't'},
+	};
+	int opt_index = 0;
+	int c = -1;
+	while((c = getopt_long(argc, argv, "o:f:n:hvt", long_opts, &opt_index)) != -1){
+		switch(c){
+			case 'o': {
+				if(!result->output){
+					int len = strlen(optarg)+1;
+					result->output = malloc(len * sizeof(char));
+					if(!result->output){
+						perror("malloc");
+						goto error;
+					}
+					memcpy(result->output, optarg, len);
+				}
+				break;
+			}
+			case 'f': {
+				if(!result->input){
+					int len = strlen(optarg)+1;
+					result->input = malloc(len * sizeof(char));
+					if(!result->input){
+						perror("malloc");
+						goto error;
+					}
+					memcpy(result->input, optarg, len);
+				}
+				break;
+			}
+			case 'n': {
+				if(!result->macro_name){
+					int len = strlen(optarg)+1;
+					result->macro_name = malloc(len * sizeof(char));
+					if(!result->macro_name){
+						perror("malloc");
+						goto error;
+					}
+					memcpy(result->macro_name, optarg, len);
+				}
+				break;
+			}
+			case 'h':
+				result->help = true;
+				break;
+			case 'v':
+				result->version = true;
+				break;
+			case 't':
+				result->http_headers = true;
+				break;
+			case '?':
+			default:
+				result->help = true;
+				break;
+		}
+	}
+	return result;
+
+error:					// Return NULL without memory leaks
+	flags_free(result);
+	return NULL;
 }
 
 void flags_free(flags_t* flags){
-    if(flags->input) free(flags->input);
-    if(flags->output) free(flags->output);
-    free(flags);
+	if(!flags) return;
+	if(flags->input) free(flags->input);
+	if(flags->output) free(flags->output);
+	if(flags->macro_name) free(flags->macro_name);
+	free(flags);
 }
 
 void print_help(char* program_name){
-    printf("%s -f file.html [opts]\n\t"\
-        "-o --output <file.h>   Specify an output file, if not used will use stdin\n\t"\
-        "-t --http-headers      Add HTTP headers in the output\n\t"\
-        "-v --version           Show installed version\n", program_name);
+	printf(
+		"Usage:\n"
+		"	%s -f <file.html> [options]\n"
+		"Options:\n"
+		"	-f, --file <file.html>		Input HTML file (required)\n"
+		"	-o, --output <file.h>		Output header file (default: stdout)\n"
+		"	-n, --macro-name			Macro name (default: derived from input file)\n"
+		"	-t, --http-headers			Add HTTP headers to output\n"\
+		"	-v, --version				Show installed version\n"
+		"	-h, --help					Display this help message\n"
+		, program_name);
 }
 
 void print_version(){
-    printf("PageTransmuter %s\n", PROGRAM_VERSION);
+	printf("PageTransmuter %s\n", PROGRAM_VERSION);
 }
--- src/main.c
+++ src/main.c
@@ -1,42 +1,96 @@
-//Headers del programa
 #include "Include/Functions.h"
 
-int main(int argc, char *argv[]) {
-    FILE* input;
-    FILE* output;
-    flags_t* flags = flags_get(argc, argv);
-    
-    if(flags->version) print_version();
-    else if(flags->help) print_help(argv[0]);
-    else if(!flags->input) fprintf(stderr, "-f flag is required\nRun %s -h for help\n", argv[0]);   // No input is specified
-    else if(!check_path(flags->input)) fprintf(stderr, "Input path is not valid\n");                // Invalid input path
-    else if (validate_html_file(flags->input)){
-        input = fopen(flags->input, "r");
-        if(!input)
-            perror("fopen");
-        if(flags->output){
-            output = fopen(flags->output, "w");
-            if(!output)
-                perror("fopen");
-        } else if(!flags->output) output = stdout;   // If no output defined, default to stdout
-        if(output) {
-            char* name = "test1";
-            long long content_length = get_content_length(flags->input);
-            if(content_length > 0) {
-                add_include_guards(output, name);
-                create_macro(output, name);
-                if(flags->http_headers)
-                    add_http_headers(output, &content_length);
-                close_include_guards(output);
-            } else fprintf(stderr, "File has no content\n");
-        } else perror("fopen");
-    }
-    flags_free(flags);
-    close_files(input, output);
-    return 0;
-}
+/* main.c - program entry point
+	- Performs necesary checks before entering main loop
+	- Handles all error messages printed to stderr
+	- The 'cleanup' tag is used to exit the program safely
+*/
 
-void close_files(FILE* input, FILE* output){
-    if(input != NULL) fclose(input);
-    if(output != NULL && output != STDIN_FILENO) fclose(output);
+int main(int argc, char *argv[]) {
+	FILE* input = NULL;
+	FILE* output = NULL;
+	flags_t* flags = flags_get(argc, argv);
+	
+	if(!flags){
+		fprintf(stderr, "Failed to parse arguments\n");
+		return -1;
+	}
+	if(flags->help) {
+		print_help(argv[0]);
+		goto cleanup;
+	}
+	if(flags->version){
+		print_version();
+		goto cleanup;
+	}
+	if(!flags->input) {
+		fprintf(stderr, "-f flag is required\nRun %s -h for help\n", argv[0]);
+		goto cleanup;
+	}
+	if(!check_path(flags->input)){
+		fprintf(stderr, "Input path is not valid\n");
+		goto cleanup;
+	}	
+	if (!validate_html_file(flags->input)){
+		fprintf(stderr, "Input file is not html or accessable from this scope\n");
+		goto cleanup;
+	}
+	input = fopen(flags->input, "r");
+	if(!input){
+		perror("fopen");
+		goto cleanup;
+	}
+	if(flags->output){
+		output = fopen(flags->output, "w");
+		if(!output){
+			perror("fopen");
+			goto cleanup;
+		}
+	} else {
+		output = stdout;   // If no output defined, default to stdout
+	}
+
+	if(!output) {
+		perror("fopen");
+		goto cleanup;
+	}
+	if(!flags->macro_name)
+		flags->macro_name = get_name_from_filepath(flags->input);
+	long long content_length = get_content_length(flags->input);
+	if(content_length <= 0){
+		fprintf(stderr, "File has no content\n");
+		goto cleanup;
+	}
+	add_include_guards(output, flags->macro_name);
+	create_macro(output, flags->macro_name);
+	if(!flags->http_headers)
+		add_http_headers(output, &content_length);
+	// Main program loop
+	for(int len = get_line_len(input); len != -1; len = get_line_len(input)){
+		char *line = malloc(len+1);
+		if(!line){
+			perror("malloc");
+			goto cleanup;
+		}
+		if(!fgets(line, len+1, input)){
+
+		}
+		char* transmuted_line = transmute_line(line, len);
+		if(!transmuted_line){
+			fprintf(stderr, "failed to allocate memory for transmuted line\n");
+			goto cleanup;
+		}
+		write_line(transmuted_line, output);
+		free(line);
+		free(transmuted_line);
+	}
+	fprintf(output, "\"\"");
+	close_include_guards(output);
+cleanup:	// Terminate program safely
+	flags_free(flags);
+	if(input)
+		fclose(input);
+	if(output && output != stdout) 
+		fclose(output);
+	return 0;
 }
